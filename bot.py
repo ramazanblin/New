ffb5f655f1fa63af6c35089b077c2c3340a740a3
ramazanblin <ramazan.84527.16.87@gmail.com>
2025-01-04 16:56:23 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,25 +3,24 @@ from flask import Flask
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler
 
-# Telegram Bot Setup
-TOKEN = os.getenv("TOKEN")
-
-# Создаем объект приложения
+# Flask-приложение
 app = Flask(__name__)
+
+# Telegram Bot
+TOKEN = os.getenv("TOKEN")
 bot_application = ApplicationBuilder().token(TOKEN).build()
 
-# Команда /start
+# Обработчик команды /start
 async def start(update: Update, context):
     await update.message.reply_text("Привет! Я ваш бот!")
 
 bot_application.add_handler(CommandHandler("start", start))
 
-# Запускаем бота
+# Асинхронный запуск Telegram-бота
 async def run_bot():
     await bot_application.initialize()
     await bot_application.start()
     await bot_application.updater.start_polling()
-    await bot_application.stop()
 
 @app.route('/')
 def home():
@@ -29,6 +28,7 @@ def home():
 
 if __name__ == "__main__":
     import asyncio
+    # Запускаем Telegram-бота и Flask-сервер
     asyncio.run(run_bot())
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
